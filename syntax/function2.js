41eee2cdacbc203f9dd12b73a379b79e6426843e
jakehdchoi <jakehdchoi@gmail.com>
2018-06-11 22:35:06 +0900

    function2 + output

--- syntax/function2.js
+++ syntax/function2.js
@@ -2,7 +2,7 @@ console.log(Math.round(1.5)); // 2
 console.log(Math.round(1.4)); // 1
 
 function sum(first, second){
-    console.log(first + second);
+    return first + second;
 }
 
-sum(2, 4);
+console.log(sum(2, 4));
