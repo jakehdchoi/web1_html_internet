ad8366b6412b4d177b1aad09a207a24e38f39fa9
jakehdchoi <jakehdchoi@gmail.com>
2018-06-14 00:40:35 +0900

    object and array

---
+++ syntax/object.js
@@ -0,0 +1,11 @@
+// array []
+var members = ['egoing', 'k8805', 'hoya'];
+console.log(members[1]); //k8805
+
+// object {}
+var roles = {
+  'programmer':'egoing',
+  'designer' : 'k8805',
+  'manager' : 'hoya'
+}
+console.log(roles.designer); //k8805
